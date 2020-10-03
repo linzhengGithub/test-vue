ef13c54b1c2a408ce002285dce3f1592d9ba8b12
linzhengGithub <alidlg9045@126.com>
2020-10-03 11:47:06 +0800

    创建3个页面

---
+++ src/views/Labels.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    标签
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Labels'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Money.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    记账
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Money'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

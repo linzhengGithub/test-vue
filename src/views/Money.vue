535ac73706f0e1202a755cfd112adef6348576a6
linzhengGithub <alidlg9045@126.com>
2020-10-03 12:15:33 +0800

    Money.vue 的布局完成

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    记账
+  <div class="nav-wrapper">
+    <div class="content">
+      记账
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +14,13 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    flex-grow: 1;
+    overflow: auto;
+  }
 </style>

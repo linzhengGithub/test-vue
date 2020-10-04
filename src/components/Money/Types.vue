aa024db650eafbce2d383e40f402537e21539055
linzhengGithub <alidlg9045@126.com>
2020-10-04 18:27:01 +0800

    完成types组件中的选中切换

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul class="types">
+    <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+    <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "Types",
+    props:['x'],
+    data(){
+      return{
+        type:'-' | '+'
+      }
+    },
+    mounted(){
+      this.type = this.x
+    },
+    methods:{
+      selectType(type){
+        this.type = type
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .types{
+    display: flex;
+    background: #c4c4c4;
+    text-align: center;
+    > li{
+      width: 50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      &.selected::after{
+        content: '';
+        position: absolute;
+        height: 4px;
+        width: 100%;
+        bottom: 0;
+        left: 0;
+        background: black;
+      }
+    }
+  }
+</style>

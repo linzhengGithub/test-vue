1d52e45259c6b5837f67e25402aea440ff0f7146
linzhengGithub <alidlg9045@126.com>
2020-10-04 18:44:39 +0800

    完成NumberPad组件计算器的输入

---
+++ src/components/Money/NumberPad.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="numberPad">
+    <div class="output">{{output}}</div>
+    <div class="buttons">
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
+      <button class="ok" @click="ok">OK</button>
+      <button @click="inputContent" class="zero">0</button>
+      <button @click="inputContent">.</button>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+  export default {
+    name: 'NumberPad',
+    data(){
+      return{
+        output : '0'
+      }
+    },
+    methods:{
+      inputContent(event){
+        const input = event.target.textContent
+        //如果输入框里面是0，就执行下面，如果要输入的是0123456789里面的一项，那么就把输入框里的0替换成输入的，后面在进行一项项加上去
+        if (this.output.length>=16){return;}
+        if(this.output === '0'){
+          if('0123456789'.indexOf(input) >= 0){
+            this.output = input
+          }else{
+            this.output += input
+          }
+          return;
+        }
+        if(this.output.indexOf('.') >= 0 && input === '.'){
+          return;
+        }
+        this.output += input
+      },
+      clear(){
+        this.output = '0'
+      },
+      remove(){
+        if(this.output.length === 1){
+          this.output = '0'
+        }else{
+          this.output = this.output.slice(0,-1)
+        }
+      },
+      ok(){
+        console.log('ok')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+
+  .numberPad {
+    .output {
+      @extend %clearFix;
+      @extend %innerShadow;
+      font-size: 36px;
+      font-family: Consolas, monospace;
+      padding: 9px 16px;
+      text-align: right;
+    }
+
+    .buttons {
+      @extend %clearFix;
+
+      > button {
+        width: 25%;
+        height: 64px;
+        float: left;
+        background: transparent;
+        border: none;
+
+        &.ok {
+          height: 128px;
+          float: right;
+        }
+
+        &.zero {
+          width: 50%;
+        }
+
+        $bg: #f2f2f2;
+
+        &:nth-child(1) {
+          background: $bg;
+        }
+
+        &:nth-child(2), &:nth-child(5) {
+          background: darken($bg, 4%);
+        }
+
+        &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+          background: darken($bg, 4*2%);
+        }
+
+        &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+          background: darken($bg, 4*3%);
+        }
+
+        &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+          background: darken($bg, 4*4%);
+        }
+
+        &:nth-child(14) {
+          background: darken($bg, 4*5%);
+        }
+
+        &:nth-child(12) {
+          background: darken($bg, 4*6%);
+        }
+      }
+    }
+  }
+</style>
